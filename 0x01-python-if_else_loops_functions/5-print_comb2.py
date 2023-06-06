c741be10408373e1518d7eb38077ad84d486e6b5
Jobs-rw <mjabush@yahoo.fr>
2023-06-06 14:02:25 -0700

    debug11

--- 0x01-python-if_else_loops_functions/5-print_comb2.py
+++ 0x01-python-if_else_loops_functions/5-print_comb2.py
@@ -1,4 +1,7 @@
 #!/usr/bin/python3
-for i in range(100):
-    print(f"{i:02d}", end="" if i == 99 else ", ")
-print()
+
+for i in range(0, 100):
+    if i == 99:
+        print('{}'.format(i))
+        break
+    print('{:02d}, '.format(i), end="")
