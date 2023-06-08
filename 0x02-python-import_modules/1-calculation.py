89b9998e1b8214872cca2cc5454e879d48c6eabe
Jobs-rw <mjabush@yahoo.fr>
2023-06-08 12:12:57 -0700

    debug12

--- 0x02-python-import_modules/1-calculation.py
+++ 0x02-python-import_modules/1-calculation.py
@@ -2,19 +2,15 @@
 # Importing specific functions from the calculator_1 module
 if __name__ == "__main__":
     from calculator_1 import add, subtract, multiply, divide
-
-# Define variables a and b
-a = 10
-b = 5
-
-# Call the imported functions with variables a and b as arguments
-sum_result = add(a, b)
-diff_result = subtract(a, b)
-prod_result = multiply(a, b)
-quot_result = divide(a, b)
-
-# Print the results
-print('{} + {} = {}'.format(a, b, sum_result))
-print('{} - {} = {}'.format(a, b, diff_result))
-print('{} * {} = {}'.format(a, b, prod_result))
-print('{} + {} = {}'.format(a, b, quot_result))
+    # Define variables a and b
+    a = 10
+    b = 5
+    # Call the imported functions with variables a and b as arguments
+    sum_result = add(a, b)
+    diff_result = subtract(a, b)
+    prod_result = multiply(a, b)
+    quot_result = divide(a, b)
+    print('{} + {} = {}'.format(a, b, sum_result))
+    print('{} - {} = {}'.format(a, b, diff_result))
+    print('{} * {} = {}'.format(a, b, prod_result))
+    print('{} + {} = {}'.format(a, b, quot_result))
