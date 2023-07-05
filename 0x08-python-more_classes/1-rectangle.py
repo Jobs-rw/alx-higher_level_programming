4ad98b95f9ce530820a15fba86836352e813a2aa
Jobs-rw <mjabush@yahoo.fr>
2023-07-05 09:55:06 -0700

    debug5

---
+++ 0x08-python-more_classes/1-rectangle.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+"""define the class"""
+
+
+class Rectangle:
+    def __init__(self, width, height)
+    self.width = width
+    self.height = height
+    if type(value) is not int:
+        raise TypeError('width must be an integer')
+    elif value < 0:
+        raise ValueError('width must be >= 0')
+        self.__width = value
+    if type(value) is not int:
+        raise TypeError('height must be an integer')
+    if value < 0:
+        raise ValueError('height must be >= 0')
+        self.__height = value
+        """checking type value and deeclaration"""
