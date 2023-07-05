29fc18fdafed4f7daba1b9a769eca822bceb4bbd
Jobs-rw <mjabush@yahoo.fr>
2023-07-05 12:46:07 -0700

    debug7

---
+++ 0x08-python-more_classes/2-rectangle.py
@@ -0,0 +1,89 @@
+#!/usr/bin/python3
+"""define the class"""
+
+
+class Rectangle:
+    """At this stage the class only creates private instance attributes by
+    taking in two arguments.
+
+    Args:
+        width (int): horizontal dimension of rectangle, defaults to 0
+        height (int): vertical dimension of rectangle, defaults to 0
+
+    """
+    def __init__(self, width=0, height=0):
+        self.width = width
+        self.height = height
+
+    @property
+    def width(self):
+        """__width getter.
+
+        Returns:
+            __width (int): horizontal dimension of rectangle
+
+        """
+        return self.__width
+
+    @width.setter
+    def width(self, value):
+        """Args:
+            value (int): horizontal dimension of rectangle
+
+        Attributes:
+            __width (int): horizontal dimension of rectangle
+
+        Raises:
+            TypeError: If `value` is not an int.
+            ValueError: If `value` is less than 0.
+
+        """
+        if not isinstance(value, int):
+            raise TypeError('width must be an integer')
+        elif value < 0:
+            raise ValueError('width must be >= 0')
+        self.__width = value
+
+    @property
+    def height(self):
+        """__height getter.
+
+        Returns:
+            __height (int): vertical dimension of rectangle
+
+        """
+        return self.__height
+
+    @height.setter
+    def height(self, value):
+        """Args:
+            value (int): vertical dimension of rectangle
+
+        Attributes:
+            __height (int): vertical dimension of rectangle
+
+        Raises:
+            TypeError: If `value` is not an int.
+            ValueError: If `value` is less than 0.
+
+        """
+        if not isinstance(value, int):
+            raise TypeError('height must be an integer')
+        elif value < 0:
+            raise ValueError('height must be >= 0')
+        self.__height = value
+
+    @area
+    def area(self):
+        """Area:
+            Area will be width * height
+
+        """
+        return self.__width * self.__height
+
+    def perimeter(self):
+        """Args:
+            perimeter will be width + height
+
+        """
+        return 2 * (self.__width + self.__height)
