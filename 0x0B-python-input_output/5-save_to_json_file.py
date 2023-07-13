ccaa2c26e13c8a07dc53d68d1d9ef67fa40fd689
Jobs-rw <mjabush@yahoo.fr>
2023-07-13 10:56:39 -0700

    debug10

--- 0x0B-python-input_output/5-save_to_json_file.py
+++ 0x0B-python-input_output/5-save_to_json_file.py
@@ -1,8 +1,13 @@
 #!/usr/bin/python3
-
-
+"""
+JSON representation of an object (string)
+"""
 import json
 
 
-def to_json_string(my_obj):
-    return json.dumps(my_obj)
+def save_to_json_file(my_obj, filename):
+    """
+    Return JSON repr of an object
+    """
+    with open(filename, "w", encoding="utf-8") as f:
+        json.dump(my_obj, f)
