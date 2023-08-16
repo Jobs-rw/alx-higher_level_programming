e17730dbf9ee4ff8d5b51cb15ad3bfc35177d5c3
Jobs-rw <mjabush@yahoo.fr>
2023-08-16 10:14:19 -0700

    debug11

--- 0x0D-SQL_introduction/4-first_table.sql
+++ 0x0D-SQL_introduction/4-first_table.sql
@@ -1,3 +1,3 @@
 -- create table first_table with its fields id and name
-CREATE TABLE IF NOT EXISTS 'first_table'('id' INT,
-'name' VARCHAR(256))
+CREATE TABLE IF NOT EXISTS 'first_table' ('id' INT,
+'name' VARCHAR(256));
