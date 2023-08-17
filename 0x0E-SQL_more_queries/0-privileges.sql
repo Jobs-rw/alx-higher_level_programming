3a79a3b6a3d0b06ca1e14f8f59563775eb3bd728
Jobs-rw <mjabush@yahoo.fr>
2023-08-17 11:05:04 -0700

    debug7

--- 0x0E-SQL_more_queries/0-privileges.sql
+++ 0x0E-SQL_more_queries/0-privileges.sql
@@ -1,4 +1,7 @@
 -- Lists all privileges of the users user_0d_1 and user_0d_2.
+
+-- list all grants for the following users
 SHOW GRANTS FOR 'user_0d_1'@'localhost';
+
 -- granting access user 
 SHOW GRANTS FOR 'user_0d_2'@'localhost';
