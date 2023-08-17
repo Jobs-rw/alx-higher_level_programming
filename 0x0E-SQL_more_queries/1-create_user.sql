8d6f854c183bce8babbe611988f5f16fc5c2bb9c
Jobs-rw <mjabush@yahoo.fr>
2023-08-17 10:27:09 -0700

    debug3

--- 0x0E-SQL_more_queries/1-create_user.sql
+++ 0x0E-SQL_more_queries/1-create_user.sql
@@ -1,3 +1,8 @@
 -- create user user_0d_1 and grant him/her all privileges on your MySQL server
-CREATE USER IF NOT EXISTS 'user_0d_1'@'localhost' IDENTIFIED BY 'user_0d_1_pwd';
-GRANT ALL PRIVILEGES ON *.* TO 'user_0d_1'@'localhost' IDENTIFIED BY 'user_0d_1_pwd';
+CREATE USER
+    IF NOT EXISTS 'user_0d_1'@'localhost'
+       	IDENTIFIED BY 'user_0d_1_pwd';
+GRANT ALL PRIVILEGES
+    ON *.*
+    TO 'user_0d_1'@'localhost'
+    IDENTIFIED BY 'user_0d_1_pwd';
