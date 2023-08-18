7501775e48c49ffdaa6f4f6a0f9ef20345184dd3
Jobs-rw <mjabush@yahoo.fr>
2023-08-18 06:19:10 -0700

    debug15

--- 0x0E-SQL_more_queries/6-states.sql
+++ 0x0E-SQL_more_queries/6-states.sql
@@ -1,3 +1,7 @@
--- create databse hbtn_0d_usa and table states.
+-- Creates the table hbtn_0d_usa with table states.
 CREATE DATABASE IF NOT EXISTS `hbtn_0d_usa`;
-CREATE TABLE IF NOT EXISTS `hbtn_0d_usa`.`states (PRIMARY KEY(`id`), `id` INT NOT NULL AUTO_INCREMENT, `name` NOT NULL VARCHAR(256)); 
+CREATE TABLE IF NOT EXISTS `hbtn_0d_usa`.`states` (
+    PRIMARY KEY(`id`),
+    `id`   INT          NOT NULL AUTO_INCREMENT,
+    `name` VARCHAR(256) NOT NULL
+); 
