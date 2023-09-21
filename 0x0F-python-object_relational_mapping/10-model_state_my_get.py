d45b9531679944901e54b0bda9b6481ec7f8cd66
jobs-rw <mjabush@yahoo.fr>
2023-09-21 20:12:02 +0000

    new release for 13-question

--- 0x0F-python-object_relational_mapping/10-model_state_my_get.py
+++ 0x0F-python-object_relational_mapping/10-model_state_my_get.py
@@ -1,27 +1,24 @@
 #!/usr/bin/python3
+""" Start link class to table in database
 """
-script that prints the State object with the name passed as argument from the database hbtn_0e_6_usa
-"""
-
+import sys
 from model_state import Base, State
-from sqlalchemy import create_engine
+
+from sqlalchemy import (create_engine)
 from sqlalchemy.orm import sessionmaker
-from sys import argv
+
 
 if __name__ == "__main__":
-    # create an engine
-    engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'.format(
-        argv[1], argv[2], argv[3]), pool_pre_ping=True)
-    # create a configured "Session" class
-    Session = sessionmaker(bind=engine)
-    # create a Session
-    session = Session()
+    engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'.
+                           format(sys.argv[1], sys.argv[2],
+                                  sys.argv[3]), pool_pre_ping=True)
     Base.metadata.create_all(engine)
-
-    s_tate = session.query(State).filter(State.name == argv[4]).first()
-
-    if s_tate:
-        print("{}".format(s_tate.id))
+    Session = sessionmaker(bind=engine)
+    session = Session()
+    states = session.query(State).filter(State.name == sys.argv[4]).all()
+    if states:
+        for state in states:
+            print("{}".format(state.id))
     else:
         print("Not found")
     session.close()
--- 0x0F-python-object_relational_mapping/13-model_state_delete_a.py
+++ 0x0F-python-object_relational_mapping/13-model_state_delete_a.py
@@ -1,22 +1,27 @@
 #!/usr/bin/python3
-""" script that deletes all State objects with a name containing the letter a from the database hbtn_0e_6_usa
 """
-import sys
+script that deletes all State objects with a name containing the letter a from the database hbtn_0e_6_usa
+"""
+
 from model_state import Base, State
-
-from sqlalchemy import (create_engine)
+from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
-
+from sys import argv
 
 if __name__ == "__main__":
-    engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'.
-                           format(sys.argv[1], sys.argv[2],
-                                  sys.argv[3]), pool_pre_ping=True)
-    Base.metadata.create_all(engine)
+    # create an engine
+    engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'.format(
+        argv[1], argv[2], argv[3]), pool_pre_ping=True)
+    # create a configured "Session" class
     Session = sessionmaker(bind=engine)
+    # create a Session
     session = Session()
-    states = session.query(State).filter(State.name.like('%a%'))
-    for state in states:
-        session.delete(state)
-    session.commit()
+    Base.metadata.create_all(engine)
+
+    s_tate = session.query(State).filter(State.name == argv[4]).first()
+
+    if s_tate:
+        print("{}".format(s_tate.id))
+    else:
+        print("Not found")
     session.close()
