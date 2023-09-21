81c8c41f8391f698f8fd3920103e80c723188bb2
jobs-rw <mjabush@yahoo.fr>
2023-09-21 17:33:57 +0000

    debug for new release

--- 0x0F-python-object_relational_mapping/7-model_state_fetch_all.py
+++ 0x0F-python-object_relational_mapping/7-model_state_fetch_all.py
@@ -1,30 +1,25 @@
 #!/usr/bin/python3
 """
-the Script that takes in the name of a state and lists
-all cities of that state, using the database
+lists all State objects from the database - Using module SQLAlchemy
 """
-import MySQLdb
+from model_state import Base, State
 from sys import argv
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
 
-# codes will  not be executed when imported
-if __name__ == '__main__':
-    # make a connection to the database
-    db = MySQLdb.connect(host="localhost", port=3306, user=argv[1],
-                         passwd=argv[2], db=argv[3])
+if __name__ == "__main__":
 
-    cur = db.cursor()
-    cur.execute("SELECT cities.id, cities.name FROM cities\
-                INNER JOIN states ON cities.state_id = states.id\
-                WHERE states.name = %s", [argv[4]])
+    # create an engine
+    engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'.format(
+        argv[1], argv[2], argv[3]), pool_pre_ping=True)
 
-    row = cur.fetchone()
-    j = []
-    while row is not None:
-        j.append(row[1])
-        row = cur.fetchone()
+    # create a configured "Session" class
+    Session = sessionmaker(bind=engine)
+    # create a Session
+    session = Session()
+    Base.metadata.create_all(engine)
 
-    print(", ".join(j))
-
-    # Clean up process
-    cur.close()
-    db.close()
+    i = session.query(State).order_by(State.id).all()
+    for state in i:
+        print("{}: {}".format(state.id, state.name))
+    session.close()
