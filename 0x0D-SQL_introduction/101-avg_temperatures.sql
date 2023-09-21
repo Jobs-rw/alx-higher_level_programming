5e8419036da9cc1f057fb6eb76995c10e523b759
jobs-rw <mjabush@yahoo.fr>
2023-09-21 09:46:44 +0000

    correction on 101-avg_temperatures.sql

--- 0x0D-SQL_introduction/101-avg_temperatures.sql
+++ 0x0D-SQL_introduction/101-avg_temperatures.sql
@@ -1,3 +1,3 @@
---list avergae temperaturesi
--- Execute: cat 101-avg_temperatures.sql | mysql -hlocalhost -uroot -p hbtn_0c_0
+-- list avergae temperaturesi
+-- cat 101-avg_temperatures.sql | mysql -hlocalhost -uroot -p
 SELECT city, AVG(value) AS avg_temp FROM temperatures GROUP BY city ORDER BY avg_temp DESC;
