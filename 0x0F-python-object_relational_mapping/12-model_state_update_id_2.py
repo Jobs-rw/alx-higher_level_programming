66864594accf8d2fdda110bab36f49ab8688e40f
jobs-rw <mjabush@yahoo.fr>
2023-09-21 19:23:58 +0000

    debug for new release

--- 0x0F-python-object_relational_mapping/12-model_state_update_id_2.py
+++ 0x0F-python-object_relational_mapping/12-model_state_update_id_2.py
@@ -1,21 +1,24 @@
 #!/usr/bin/python3
-""" script that changes the name of a State object from the database hbtn_0e_6_usa
 """
-import sys
+script that changes the name of a State object from the database hbtn_0e_6_usa
+"""
+
 from model_state import Base, State
-
-from sqlalchemy import (create_engine)
+from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
-
+from sys import argv
 
 if __name__ == "__main__":
-    engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'.
-                           format(sys.argv[1], sys.argv[2],
-                                  sys.argv[3]), pool_pre_ping=True)
-    Base.metadata.create_all(engine)
+    # create an engine
+    engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'.format(
+        argv[1], argv[2], argv[3]), pool_pre_ping=True)
+    # create a configured "Session" class
     Session = sessionmaker(bind=engine)
+    # create a Session
     session = Session()
-    state = session.query(State).filter(State.id == 2)
-    state.update({State.name: 'New Mexico'})
+    Base.metadata.create_all(engine)
+    state_update = session.query(State).filter_by(id='2').first()
+    state_update.name = "New Mexico"
+    # commit and close session
     session.commit()
     session.close()
