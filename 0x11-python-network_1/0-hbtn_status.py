acace47e8974c91012e1df754d9683b63d541e1c
jobs-rw <mjabush@yahoo.fr>
2023-09-29 14:13:16 +0000

    updates for new release with comments

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -4,15 +4,14 @@
 
 
 if __name__ == "__main__":
-import urllib.request
+    import urllib.request
 
-url = "https://alx-intranet.hbtn.io/status"
-
-try:
-    with urllib.request.urlopen(url) as response:
-        html = response.read()
-        print("Body response:")
-        print("\t- type :", type(html))
-        print("\t- content :", html.decode('utf-8'))
-except urllib.error.HTTPError as e:
-    print("Error: {} {}".format(e.code, e.reason))
+    url = "https://alx-intranet.hbtn.io/status"
+    try:
+        with urllib.request.urlopen(url) as response:
+            html = response.read()
+            print("Body response:")
+            print("\t- type :", type(html))
+            print("\t- content :", html.decode('utf-8'))
+    except urllib.error.HTTPError as e:
+        print("Error: {} {}".format(e.code, e.reason))
